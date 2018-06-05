099adfd3aba671c901defb8fd02d9a52f7f65ba0
genghongyu <genghongyu@gomeholdings.com>
2018-06-05 15:48:10 +0800

    init

--- rate-limiter-demo/src/test/java/com/gomefinance/rate/limiter/test/AppTest.java
+++ rate-limiter-demo/src/test/java/com/gomefinance/rate/limiter/test/AppTest.java
@@ -3,17 +3,15 @@ package com.gomefinance.rate.limiter.test;
 import com.gomefinance.rate.limiter.demo.DemoApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Created by Administrator on 2018/6/5.
  */
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoApplication.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {DemoApplication.class})
 public class AppTest {
     @Test
     public void theTest() {
